1f408b32ce0d8cf33d9ee30ed57b41b996e5d2e0
Roman <romanbelinsky99@gmail.com>
2019-02-12 00:02:03 +0600

    Create header for BoxStructProcessor

---
+++ headers/BoxStructProcessor.h
@@ -0,0 +1,10 @@
+#ifndef CPP_TASKS_BOXSTRUCTPROCESSOR_H
+#define CPP_TASKS_BOXSTRUCTPROCESSOR_H
+
+#include "BoxStruct.h"
+
+int boxPrice(Box boxes[]);
+bool isSizeAllBoxesSmaller(Box boxes[], int maxLength, int maxWidth, int maxWeight);
+double maxWeightSmallerMaxV(double maxV);
+
+#endif
