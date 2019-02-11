a16e3970ee22325ec7e52e7b7dc4e97ad6517ccf
Roman <romanbelinsky99@gmail.com>
2019-02-11 23:35:16 +0600

    Create constructor for Box

---
+++ src/BoxStruct.cpp
@@ -0,0 +1,7 @@
+#include "../headers/BoxStruct.h"
+
+Box::Box(int newLength, int newWidth, int newHeight, double newWeight, int newValue) : length(newLength),
+                                                                                       width(newWidth),
+                                                                                       height(newHeight),
+                                                                                       weight(newWeight),
+                                                                                       value(newValue) {}
