8387c82f880fd42b0e182ea787701b2808274677
Roman <romanbelinsky99@gmail.com>
2019-02-25 16:29:52 +0600

    Add constructor, destructor, getters and setters

--- headers/Container.h
+++ headers/Container.h
@@ -1,8 +1,48 @@
 #ifndef CPP_TASKS_CONTAINER_H
 #define CPP_TASKS_CONTAINER_H
 
-class Container {
+#include <vector>
+#include "../headers/BoxClass.h"
 
+using namespace std;
+using namespace BoxClass;
+
+class Container {
+private:
+    vector<Box> container;
+    int length;
+    int width;
+    int height;
+    int maxWeight;
+    int currentWeight;
+
+public:
+    void setLength(int length);
+
+    void setWidth(int width);
+
+    void setHeight(int height);
+
+    void setMaxWeight(int maxWeight);
+
+    void setCurrentWeight(int currentWeight);
+
+public:
+    Container(const int length, const int width, const int height, const int maxWeight);
+
+    const vector<Box> &getContainer() const;
+
+    int getLength() const;
+
+    int getWidth() const;
+
+    int getHeight() const;
+
+    int getMaxWeight() const;
+
+    int getCurrentWeight() const;
+
+    virtual ~Container() = default;
 };
 
 #endif
--- src/Container.cpp
+++ src/Container.cpp
@@ -1,1 +1,54 @@
 #include "../headers/Container.h"
+
+Container::Container(const int length, const int width, const int height, const int maxWeight) {
+    setLength(length);
+    setWidth(width);
+    setHeight(height);
+    setMaxWeight(maxWeight);
+
+    currentWeight = 0;
+}
+
+const vector<Box> &Container::getContainer() const {
+    return container;
+}
+
+int Container::getLength() const {
+    return length;
+}
+
+int Container::getWidth() const {
+    return width;
+}
+
+int Container::getHeight() const {
+    return height;
+}
+
+int Container::getMaxWeight() const {
+    return maxWeight;
+}
+
+int Container::getCurrentWeight() const {
+    return currentWeight;
+}
+
+void Container::setLength(int length) {
+    Container::length = length;
+}
+
+void Container::setWidth(int width) {
+    Container::width = width;
+}
+
+void Container::setHeight(int height) {
+    Container::height = height;
+}
+
+void Container::setMaxWeight(int maxWeight) {
+    Container::maxWeight = maxWeight;
+}
+
+void Container::setCurrentWeight(int currentWeight) {
+    Container::currentWeight = currentWeight;
+}
