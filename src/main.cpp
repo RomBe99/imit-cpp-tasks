513f4a629163d5f95ae9c00eb61b261544299d63
Roman <romanbelinsky99@gmail.com>
2019-03-04 16:55:51 +0600

    Add test

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,18 @@
-#include "../headers/BoxStruct.h"
+#include "../headers/BoxClass.h"
+#include "../headers/BoxClassProcessor.h"
+#import <iostream>
 
 int main() {
-    return 0;
+    Box *box1 = new Box(1, 1, 1, 1, 1);
+    Box *box2 = new Box(2, 2, 2, 2, 2);
+    Box *box3 = new Box(3, 3, 3, 3, 3);
+    Box *box4 = new Box(4, 4, 4, 4, 4);
+    Box *box5 = new Box(5, 5, 5, 5, 5);
+    Box *boxArr = new Box[5];
+    boxArr[0] = *box1;
+    boxArr[1] = *box2;
+    boxArr[2] = *box3;
+    boxArr[3] = *box4;
+    boxArr[4] = *box5;
+    std::cout << BoxClassProcessor::isAllBox(5, boxArr);
 }
