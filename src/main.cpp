055f0067955e983f0a07da3fc02f3d858c1ef1e0
Roman <romanbelinsky99@gmail.com>
2019-04-28 01:51:36 +0600

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,7 @@
 
 #include "../headers/BoxClass.h"
 #include "../headers/BoxClassProcessor.h"
-#include "../headers/TestDynamicArrayInt.h"
 
 int main() {
-    DynamicArrayInt a = DynamicArrayInt(10);
-    std::cout << testarrayint::assignmentOperatorTest(a);
     return 0;
 }
