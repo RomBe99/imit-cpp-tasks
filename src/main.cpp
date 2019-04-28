210c9a6a7cee9d0fc1eb154785207ead506e21d1
Roman <romanbelinsky99@gmail.com>
2019-04-28 11:34:40 +0600

    Add tests in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
 
-#include "../headers/BoxClass.h"
-#include "../headers/BoxClassProcessor.h"
+#include "../headers/DynamicArrayInt.h"
+
+void testDynamicArrayInt() {
+    // Тест конструктора по умолчанию
+    DynamicArrayInt defaultConstructor;
+
+    // Тест конструктора по размеру
+    const int SIZE1 = 4;
+    DynamicArrayInt sizeConstructor(SIZE1);
+
+    // Тест конструктора по размеру и числу
+    const int SIZE2 = 5;
+    const int NUMBER = 33;
+    DynamicArrayInt sizeAndNumberConstructor(SIZE2, NUMBER);
+
+    // Тест конструктора копирования и оператора присваивания
+    DynamicArrayInt copyConstructor(sizeAndNumberConstructor);
+    std::cout << &copyConstructor << "   " << &sizeAndNumberConstructor << std::endl;
+    std::cout << copyConstructor << "   " << sizeAndNumberConstructor << std::endl;
+
+    copyConstructor = defaultConstructor;
+    std::cout << &copyConstructor << "   " << &defaultConstructor << std::endl;
+    std::cout << copyConstructor << "   " << defaultConstructor << std::endl;
+
+    // Тест конструктора перемещения и оператора перемещения
+    DynamicArrayInt moveConstructor = std::move(defaultConstructor);
+//    std::cout << &moveConstructor << "   " << &defaultConstructor << std::endl; // Адреса объектов одинаковые, но вот далее
+//    std::cout << moveConstructor << "   " << defaultConstructor << std::endl; // Выбросит сегфолт, так как пытаемся обратится к несуществующему объекту
+}
 
 int main() {
+    testDynamicArrayInt();
     return 0;
 }
