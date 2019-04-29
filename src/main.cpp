e90a92ac2ef80035f89522d2c682fbf4f8d9ac0d
Roman <romanbelinsky99@gmail.com>
2019-04-29 15:44:58 +0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,25 +4,28 @@
 
 void testDynamicArrayInt() {
     // Тест конструктора по умолчанию
-    DynamicArrayInt defaultConstructor;
+//    DynamicArrayInt defaultConstructor;
+//    std::cout << defaultConstructor;
 
     // Тест конструктора по размеру
-    const int SIZE1 = 4;
-    DynamicArrayInt sizeConstructor(SIZE1);
+//    const int SIZE1 = 4;
+//    DynamicArrayInt sizeConstructor(SIZE1);
+//    std::cout << sizeConstructor;
 
     // Тест конструктора по размеру и числу
-    const int SIZE2 = 10;
-    const int NUMBER = 33;
-    DynamicArrayInt sizeAndNumberConstructor(SIZE2, NUMBER);
+//    const int SIZE2 = 10;
+//    const int NUMBER = 33;
+//    DynamicArrayInt sizeAndNumberConstructor(SIZE2, NUMBER);
+//    std::cout << sizeAndNumberConstructor;
 
     // Тест конструктора копирования и оператора присваивания
-    DynamicArrayInt copyConstructor(sizeAndNumberConstructor);
-    std::cout << &copyConstructor << "   " << &sizeAndNumberConstructor << std::endl;
-    std::cout << copyConstructor << "   " << sizeAndNumberConstructor << std::endl;
+//    DynamicArrayInt copyConstructor(sizeAndNumberConstructor);
+//    std::cout << &copyConstructor << "   " << &sizeAndNumberConstructor << std::endl;
+//    std::cout << copyConstructor << "   " << sizeAndNumberConstructor << std::endl;
 
-    copyConstructor = defaultConstructor;
-    std::cout << &copyConstructor << "   " << &defaultConstructor << std::endl;
-    std::cout << copyConstructor << "   " << defaultConstructor << std::endl;
+//    copyConstructor = defaultConstructor;
+//    std::cout << &copyConstructor << "   " << &defaultConstructor << std::endl;
+//    std::cout << copyConstructor << "   " << defaultConstructor << std::endl;
 
     // Тест конструктора перемещения и оператора перемещения
 //    DynamicArrayInt moveConstructor = std::move(defaultConstructor);
@@ -32,22 +35,22 @@ void testDynamicArrayInt() {
     // Тест операций сравнения
 //    std::cout << (defaultConstructor == copyConstructor) << std::endl;
 //    std::cout << (defaultConstructor != copyConstructor) << std::endl;
-//    std::cout << (defaultConstructor <= copyConstructor) << std::endl;
-//    std::cout << (defaultConstructor >= copyConstructor) << std::endl;
+//    std::cout << (defaultConstructor < copyConstructor) << std::endl;
+//    std::cout << (defaultConstructor > copyConstructor) << std::endl;
 //    std::cout << (sizeAndNumberConstructor > defaultConstructor) << std::endl;
 
     // Тест сложения массивов
-    DynamicArrayInt arrSum = sizeAndNumberConstructor + defaultConstructor;
-    std::cout << arrSum << std::endl;
+//    DynamicArrayInt arrSum = sizeAndNumberConstructor + defaultConstructor;
+//    std::cout << arrSum << std::endl;
 
     // Тест резерва
-    const int NEW_SIZE = 15;
-    arrSum.resize(NEW_SIZE);
-    std::cout << arrSum << arrSum.capacity() << std::endl;
-    arrSum.pushBack(NEW_SIZE);
-    std::cout << arrSum << arrSum.capacity() << std::endl;
-    std::cout << arrSum << arrSum.popBack() << ' ' << arrSum.capacity() << std::endl;
-    std::cout << arrSum;
+//    const int NEW_SIZE = 15;
+//    arrSum.resize(NEW_SIZE);
+//    std::cout << arrSum << arrSum.capacity() << std::endl;
+//    arrSum.pushBack(NEW_SIZE);
+//    std::cout << arrSum << arrSum.capacity() << std::endl;
+//    std::cout << arrSum.popBack() << ' ' << arrSum.capacity() << std::endl;
+//    std::cout << arrSum;
 }
 
 int main() {
