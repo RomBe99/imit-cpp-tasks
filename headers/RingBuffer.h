eaedcb539c90a8ab4893cbecdd4bce48270181e3
Roman <romanbelinsky99@gmail.com>
2019-05-06 01:36:37 +0600

    Implement class RingBuffer

--- headers/RingBuffer.h
+++ headers/RingBuffer.h
@@ -1,8 +1,51 @@
 #ifndef CPP_TASKS_RINGBUFFER_H
 #define CPP_TASKS_RINGBUFFER_H
 
-class RingBuffer {
+#include <cstddef>
 
+class RingBuffer {
+private:
+    struct RingBufferElement {
+        bool isEmpty = true;
+        int value = 0;
+    };
+
+    RingBufferElement *buffer;
+    std::size_t bufferSize;
+
+    RingBufferElement *head;
+    RingBufferElement *tail;
+
+    bool bufferIsEmpty = true;
+    bool bufferIsFull = false;
+
+    unsigned int in = 0;
+    unsigned int out = 0;
+
+    void setBufferSize(size_t size);
+
+    void postIncrementIn();
+
+    void postIncrementOut();
+
+public:
+    explicit RingBuffer(size_t bufferSize);
+
+    void pushBack(int value);
+
+    int popFirst();
+
+    int getFirst() const;
+
+    void clear();
+
+    size_t size() const;
+
+    bool isEmpty() const;
+
+    bool isFull() const;
+
+    ~RingBuffer();
 };
 
 #endif
--- src/RingBuffer.cpp
+++ src/RingBuffer.cpp
@@ -1,1 +1,88 @@
 #include "../headers/RingBuffer.h"
+
+void RingBuffer::setBufferSize(const size_t newSize) {
+    bufferSize = newSize;
+}
+
+RingBuffer::RingBuffer(const size_t bufferSize) {
+    setBufferSize(bufferSize);
+
+    buffer = new RingBufferElement[bufferSize];
+
+    head = buffer;
+    tail = head + bufferSize - 1;
+}
+
+void RingBuffer::pushBack(const int value) {
+    if (bufferIsFull) {
+        // TODO Сделать Exception для переполнения
+    }
+
+    if (bufferIsEmpty) {
+        bufferIsEmpty = false;
+    }
+
+    buffer[in].isEmpty = false;
+    buffer[in].value = value;
+
+    postIncrementIn();
+}
+
+int RingBuffer::popFirst() {
+    return 0;
+}
+
+int RingBuffer::getFirst() const {
+    return 0;
+}
+
+void RingBuffer::postIncrementIn() {
+    if (in < bufferSize) {
+        in++;
+    } else {
+        in = 0;
+    }
+}
+
+void RingBuffer::postIncrementOut() {
+    if (out < bufferSize) {
+        out++;
+    } else {
+        out = 0;
+    }
+}
+
+void RingBuffer::clear() {
+    if (bufferIsEmpty) {
+        return;
+    }
+
+    if (bufferIsFull) {
+        bufferIsFull = false;
+    }
+
+    bufferIsEmpty = true;
+    in = 0;
+    out = 0;
+
+    for (int i = 0; i < bufferSize; i++) {
+        buffer[i].isEmpty = false;
+        buffer[i].value = 0;
+    }
+}
+
+size_t RingBuffer::size() const {
+    return bufferSize;
+}
+
+bool RingBuffer::isEmpty() const {
+    return bufferIsEmpty;
+}
+
+bool RingBuffer::isFull() const {
+    return bufferIsFull;
+}
+
+RingBuffer::~RingBuffer() {
+    delete[]buffer;
+}
