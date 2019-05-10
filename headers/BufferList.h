d7c07ed4139cfd2e84cad9a0aef01223fdfbb6fb
Roman <romanbelinsky99@gmail.com>
2019-05-10 20:52:25 +0600

    Add Javadoc, override from abstract class List

--- headers/BufferList.h
+++ headers/BufferList.h
@@ -3,18 +3,55 @@
 
 #include "List.h"
 
-template <typename T>
+/**
+ * Класс двунапрвленного кольцевого списка с буферным элементом.
+ *
+ * @tparam T любой примитивный тип данных.
+ */
+template<typename T>
 class BufferList : public List<T> {
 private:
-    class ListElement {
-    public:
-        ListElement *nextElement = nullptr;
-        T value;
-        ListElement *previousElement = nullptr;
 
-        explicit ListElement(const T value);
-        ~ListElement();
+    /**
+     * Класс элемента списка.
+     */
+    class BidirectionalListElement {
+    public:
+        BidirectionalListElement *nextElement = nullptr;
+        T value = 0;
+        BidirectionalListElement *previousElement = nullptr;
+
+        BidirectionalListElement() = default;
+
+        explicit BidirectionalListElement(T value);
+
+        ~BidirectionalListElement();
     };
+
+    BidirectionalListElement *bufferElement = new BidirectionalListElement(); // next - начало, prev - конец
+    int listSize = 0;
+    const static int DEFAULT_SIZE = 10;
+
+public:
+    BufferList();
+
+    explicit BufferList(int size);
+
+    void insert(T value, const Iterator<List<T>> &iterator) override;
+
+    void deleteElement(const Iterator<List<T>> &iterator) override;
+
+    Iterator<T> &firstEnter(T value) override;
+
+    void clear() override;
+
+    bool isEmpty() override;
+
+    int size() override;
+
+    Iterator<T> &begin() override;
+
+    ~BufferList();
 };
 
 #endif
