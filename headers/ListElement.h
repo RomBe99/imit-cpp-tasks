381befeaa9ec9ac934bb2c418c867d51e80f6bd1
Roman <romanbelinsky99@gmail.com>
2019-05-13 23:09:21 +0600

    Add Javadoc for abstract class ListElement

--- headers/ListElement.h
+++ headers/ListElement.h
@@ -1,9 +1,20 @@
 #ifndef CPP_TASKS_LISTELEMENT_H
 #define CPP_TASKS_LISTELEMENT_H
 
+/**
+ * Абстрактный класс для элемента структуры данных.
+ *
+ * @tparam T любой примитивный тип данных, который будет хранить наследуемый класс.
+ */
 template<typename T>
 class ListElement {
 public:
+
+    /**
+     * Возвращает значение которое хранит элемент.
+     *
+     * @return значение которое хранит элемент.
+     */
     virtual T getValue() const = 0;
 };
 
