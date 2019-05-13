69102283658e9ad0da77b036e924570a73aab1b4
Roman <romanbelinsky99@gmail.com>
2019-05-13 23:48:30 +0600

    Add Javadoc for abstract class Iterator

--- headers/Iterator.h
+++ headers/Iterator.h
@@ -3,12 +3,25 @@
 
 #include "List.h"
 
-template <class T>
+/**
+ * Класс абстракного итератора.
+ */
 class Iterator {
 public:
+    /**
+     * Метод для начала итерации структуры данных.
+     */
     virtual void start() = 0;
-    virtual T *getElement() const = 0;
+
+    /**
+     * Метод для смещения итератора на соедующую позицию.
+     */
     virtual void next() = 0;
+
+    /**
+     * Метод для проверки окончани итерации по структуре данных.
+     * @return true - если пройдёны все элементы структуры данных.
+     */
     virtual bool isFinish() const = 0;
 };
 
