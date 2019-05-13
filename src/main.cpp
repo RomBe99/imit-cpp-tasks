606142e10f516a30443b7f310ef9a445d24b7b36
Roman <romanbelinsky99@gmail.com>
2019-05-13 21:56:43 +0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,8 @@
 #include <iostream>
 
-#include "../headers/RingBuffer.h"
-#include "../headers/RingBufferIterator.h"
-
 #include "../headers/BufferList.h"
 
 int main() {
-//    const std::size_t BUFFER_SIZE = 5;
-//
-//    auto *r = new RingBuffer(BUFFER_SIZE);
-//    auto *i = new RingBufferIterator(r);
-//
-//    r->pushBack(22);
-//    r->pushBack(33);
-//    r->pushBack(44);
-//    r->pushBack(56);
-//    r->pushBack(77);
-//
-//    while (!i->finish() && !i->isEmpty()) {
-//        std::cout << i->getValue() << ' ';
-//        i->next();
-//    }
-//
-//    std::cout << std::endl;
-//
-//    i->start();
-//
-//    while (!i->finish() && !i->isEmpty()) {
-//        std::cout << i->getValue() << ' ';
-//        i->next();
-//    }
-//
-//    i->start();
-//
-//    std::cout << std::endl;
-//
-//    std::cout << r->popFirst() << std::endl;
-//    std::cout << r->popFirst() << std::endl;
-//
-//    i->next();
-//    i->next();
-//
-//    while (!i->finish() && !i->isEmpty()) {
-//        std::cout << i->getValue() << ' ';
-//        i->next();
-//    }
-
     auto b = new BufferList<int>(5);
     return 0;
 }
