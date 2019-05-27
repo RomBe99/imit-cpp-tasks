483e04b079a62a93fc1bad3f5b55a7f6b7cdc9a9
Roman <romanbelinsky99@gmail.com>
2019-05-27 15:03:02 +0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,8 @@
 #include <iostream>
 
-#include "../headers/BufferList.h"
+#include "../headers/HashTable.h"
 
 int main()
 {
-    const size_t SIZE = 10;
-
-    auto b = new BufferList<int>(SIZE);
-    auto bi = b -> begin();
-
-    // Тест итератора
-
-    const int POSITION = 4;
-    const int VALUE = 22;
-
-    // Замена элемента
-
-    for (int i = 1; !bi -> isFinish(); i++) {
-        std::cout << i << std::endl;
-        b -> setValue(i, *bi);
-        bi -> next();
-    }
-
-    // Вставка элемента
-
-    for (int i = 0; i < POSITION; i++) {
-        bi -> next();
-    }
-
-    b -> insert(VALUE, *bi);
-
-    bi -> start();
-
-    // Тест поиска первого вхождения определённого значения и удаление элемента списка
-
-    auto fe = b -> firstEnter(VALUE);
-
-    b -> deleteElement(*fe);
-
     return 0;
 }
