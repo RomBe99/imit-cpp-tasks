199df51451d449831a0cf61f519cb27cce148a0b
Roman <romanbelinsky99@gmail.com>
2019-06-08 20:48:53 +0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,72 @@
 #include <iostream>
+#include <vector>
 
 #include "../headers/BinaryTree.h"
 
 int main()
 {
-    auto bt = new BinaryTree(5);
+    auto bt = new BinaryTree(10);
 
-    const int SIZE_ELEM1 = 1;
-    int elem1[] = {0};
+    const int SIZE_ELEM9 = 1;
+    const int ELEM9[] = {0};
 
-    const int SIZE_ELEM3 = 2;
-    int elem3[] = {0, 0};
+    const int SIZE_ELEM_MINUS20 = 2;
+    const int ELEM_MINUS20[] = {0, 0};
 
-    const int SIZE_ELEM4 = 2;
-    int elem4[] = {0, 1};
+    const int SIZE_ELEM_MINUS7 = 2;
+    const int ELEM_MINUS7[] = {0, 1};
 
-    const int SIZE_ELEM5 = 2;
-    int elem5[] = {1, 0};
+    const int SIZE_ELEM20 = 2;
+    const int ELEM20[] = {1, 0};
 
-    const int SIZE_ELEM7 = 2;
-    int elem7[] = {1, 0};
+    const int SIZE_ELEM11 = 2;
+    const int ELEM11[] = {1, 0};
 
-    const int SIZE_ELEM9 = 3;
-    int elem9[] = {1, 0, 0};
+    const int SIZE_ELEM88 = 3;
+    const int ELEM88[] = {1, 0, 0};
 
-    const int SIZE_ELEM8 = 2;
-    int elem8[] = {1, 1};
+    const int SIZE_ELEM30 = 2;
+    const int ELEM30[] = {1, 1};
 
-    const int SIZE_ELEM2 = 3;
-    int elem2[] = {1, 1, 0};
+    const int SIZE_ELEM77 = 3;
+    const int ELEM77[] = {1, 1, 0};
 
-    const int SIZE_ELEM11 = 3;
-    int elem11[] = {1, 1, 1};
+    const int SIZE_ELEM41 = 3;
+    const int ELEM41[] = {1, 1, 1};
 
-    const int SIZE_ELEM12 = 4;
-    int elem12[] = {1, 1, 1, 0};
+    const int SIZE_ELEM_MINUS1 = 4;
+    const int MINUS1[] = {1, 1, 1, 0};
 
-    bt -> insert(1, SIZE_ELEM1, elem1);
-    bt -> insert(3, SIZE_ELEM3, elem3);
-    bt -> insert(4, SIZE_ELEM4, elem4);
-    bt -> insert(5, SIZE_ELEM5, elem5);
-    bt -> insert(7, SIZE_ELEM7, elem7);
-    bt -> insert(9, SIZE_ELEM9, elem9);
-    bt -> insert(8, SIZE_ELEM8, elem8);
-    bt -> insert(2, SIZE_ELEM2, elem2);
-    bt -> insert(11, SIZE_ELEM11, elem11);
-    bt -> insert(12, SIZE_ELEM12, elem12);
+    bt -> insert(9, SIZE_ELEM9, ELEM9);
+    bt -> insert(-20, SIZE_ELEM_MINUS20, ELEM_MINUS20);
+    bt -> insert(-7, SIZE_ELEM_MINUS7, ELEM_MINUS7);
+    bt -> insert(20, SIZE_ELEM20, ELEM20);
+    bt -> insert(11, SIZE_ELEM11, ELEM11);
+    bt -> insert(88, SIZE_ELEM88, ELEM88);
+    bt -> insert(30, SIZE_ELEM30, ELEM30);
+    bt -> insert(77, SIZE_ELEM77, ELEM77);
+    bt -> insert(41, SIZE_ELEM41, ELEM41);
+    bt -> insert(-1, SIZE_ELEM_MINUS1, MINUS1);
 
     bt -> print();
 
     std::cout << "Tree contains only positive numbers? " << bt -> isPositive() << std::endl;
     std::cout << "Tree average = " << bt -> average() << std::endl;
     std::cout << "Is binary search tree? " << bt -> isBinarySearchTree() << std::endl;
+    std::cout << "Number of even numbers = " << bt -> numberOfEvenNumbers() << std::endl;
 
     bt -> deleteAllLeafs();
 
     bt -> print();
+
+    std::cout << "Tree contains only positive numbers? " << bt -> isPositive() << std::endl;
+    std::cout << "Tree average = " << bt -> average() << std::endl;
+    std::cout << "Is binary search tree? " << bt -> isBinarySearchTree() << std::endl;
+    std::cout << "Number of even numbers = " << bt -> numberOfEvenNumbers() << std::endl;
+
+    for (int i : *bt -> findRoute(11)) {
+        std::cout << i << ' ';
+    }
 
     return 0;
 }
